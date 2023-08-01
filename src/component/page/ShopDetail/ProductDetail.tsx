11928b9d896e3a62c94f74589a3f7ded67c16c9e
vuthinhung1005 <nhungvt19868@fpt.edu.vn>
2023-08-01 17:15:31 +0700

    update UI productdetail

--- src/component/page/ShopDetail/ProductDetail.tsx
+++ src/component/page/ShopDetail/ProductDetail.tsx
@@ -3,6 +3,18 @@ import React from 'react'
 const ProductDetail = () => {
     return (
         <>
+
+            <div className="container-fluid bg-secondary mb-5">
+                <div className="d-flex flex-column align-items-center justify-content-center" style={{ minHeight: "300px" }} >
+                    <h1 className="font-weight-semi-bold text-uppercase mb-3">Shop Detail</h1>
+                    <div className="d-inline-flex">
+                        <p className="m-0"><a href="/">Home</a></p>
+                        <p className="m-0 px-2">-</p>
+                        <p className="m-0">Shop Detail</p>
+                    </div>
+                </div>
+
+            </div>
             <div className="container-fluid py-5">
                 <div className="row px-xl-5">
                     <div className="col-lg-5 pb-5">
