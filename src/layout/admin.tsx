f451d305bfec2a627266da79b2f6a261a8bdfa97
Đào Xuân Mai - FE <118628374+mailuoicode@users.noreply.github.com>
2023-08-05 01:27:38 +0700

    update-product

--- src/layout/admin.tsx
+++ src/layout/admin.tsx
@@ -437,8 +437,14 @@ const AdminLayout: React.FC = () => {
                           </a>
                         </li>
                         <li className="nav-item">
-                          <a className="nav-link" href="pages/carousel.html">
-                            Carousel
+                          <a className="nav-link">
+                            <Link
+                              to="product/update"
+                              style={{ textDecoration: "none", color: "white" }}
+                            >
+                              Update Products{" "}
+                            </Link>
+                            <span className="badge badge-secondary">New</span>
                           </a>
                         </li>
                         <li className="nav-item">
